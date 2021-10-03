383b8e8cb238edd328479ebc268c42741c64ab22
Erwan ROUSSEL <e-roussel@outlook.com>
2021-10-03 22:45:57 +0200

    Refactor block.go and main.go to fit new PoW algorithm

--- main.go
+++ main.go
@@ -1,60 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"strconv"
+
+	"github.com/dimensi0n/chainpagne/blockchain"
 )
 
-/*type Data struct {
-	BottleId int
-	OwnerId  int
-}*/
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-type BlockChain struct {
-	Blocks []*Block
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	// This will join our previous block's relevant information with the new blocks
-	hash := sha256.Sum256(info)
-	// This will hash the information and return a hash
-	b.Hash = hash[:]
-	// This will set the hash of the block to the hash of the information
-	// [:] refers to golang slices and this is how you get the underlying array of the slice
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	// Refers to pointer's syntax and this is how you create a pointer to a block
-	block.DeriveHash()
-
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func initBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := initBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block")
 	chain.AddBlock("Second Block")
@@ -65,5 +19,9 @@ func main() {
 		fmt.Printf("Previous hash: %x\n", block.PrevHash)
 		fmt.Printf("data: %s\n", block.Data)
 		fmt.Printf("hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
