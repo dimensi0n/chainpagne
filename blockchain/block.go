ae2490b8205550337b4c6bf6f8e59753f8854227
Erwan ROUSSEL <e-roussel@outlook.com>
2021-10-05 09:03:11 +0200

    Add persistence

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,13 +1,19 @@
 package blockchain
 
-/*type Data struct {
-	BottleId int
-	OwnerId  int
-}*/
+import (
+	"gorm.io/gorm"
+)
+
+type Data struct {
+	BottleId string
+	OwnerId  string
+}
 
 type Block struct {
+	gorm.Model
 	Hash     []byte
-	Data     []byte
+	BottleId []byte
+	OwnerId  []byte
 	PrevHash []byte
 	Nonce    int
 }
@@ -16,8 +22,8 @@ type BlockChain struct {
 	Blocks []*Block
 }
 
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+func CreateBlock(bottleId string, ownerID string, prevHash []byte) *Block {
+	block := &Block{Hash: []byte{}, BottleId: []byte(bottleId), OwnerId: []byte(ownerID), PrevHash: prevHash, Nonce: 0}
 	// Refers to pointer's syntax and this is how you create a pointer to a block
 	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
@@ -28,14 +34,15 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
+func (chain *BlockChain) AddBlock(data *Data) *Block {
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
+	new := CreateBlock(data.BottleId, data.OwnerId, prevBlock.Hash)
 	chain.Blocks = append(chain.Blocks, new)
+	return new
 }
 
 func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
+	return CreateBlock("Genesis bottle", "Genesis owner", []byte{})
 }
 
 func InitBlockChain() *BlockChain {
